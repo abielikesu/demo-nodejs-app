c10bf4f3fab4975eb5dc2147b3b7b2b7aa8a4160
Abidis Solano <abielikesu@yahoo.com>
2022-10-29 08:54:06 +0100

    Adding form and body parser

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello from App Engine!');
+});
+app.get('/submit', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/form.html'));
+});
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+  res.send('Thanks for your message!');
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
